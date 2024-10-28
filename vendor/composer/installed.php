66aa968e97a79811ce7de7fed593d0b310718a2a
Bikra123 <bikraabn@gmail.com>
2024-10-28 20:15:42 +0700

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'user/mevec',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9c1c6743cff4d999ce32e88252554704dbd6af2a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'user/mevec' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9c1c6743cff4d999ce32e88252554704dbd6af2a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
