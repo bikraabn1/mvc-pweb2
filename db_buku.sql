3c3dcd4e47c6bfafed8a91366db28564a36e4d56
Bikra123 <bikraabn@gmail.com>
2024-10-28 19:50:05 +0700

    adding db

---
+++ db_buku.sql
@@ -0,0 +1,74 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for db_buku
+CREATE DATABASE IF NOT EXISTS `db_buku` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_buku`;
+
+-- Dumping structure for table db_buku.buku
+CREATE TABLE IF NOT EXISTS `buku` (
+  `id_buku` int NOT NULL AUTO_INCREMENT,
+  `judul_buku` varchar(100) NOT NULL,
+  `penulis` varchar(50) NOT NULL,
+  `tahun_terbit` year NOT NULL,
+  `id_kategori` int NOT NULL,
+  PRIMARY KEY (`id_buku`),
+  UNIQUE KEY `id_kategori` (`id_kategori`),
+  CONSTRAINT `kategori_buku` FOREIGN KEY (`id_kategori`) REFERENCES `kategori_buku` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table db_buku.buku: ~0 rows (approximately)
+
+-- Dumping structure for table db_buku.kategori_buku
+CREATE TABLE IF NOT EXISTS `kategori_buku` (
+  `id_kategori` int NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_kategori`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table db_buku.kategori_buku: ~0 rows (approximately)
+
+-- Dumping structure for table db_buku.peminjaman
+CREATE TABLE IF NOT EXISTS `peminjaman` (
+  `id_peminjaman` int NOT NULL AUTO_INCREMENT,
+  `nama_peminjam` varchar(50) NOT NULL,
+  `tanggal_peminjaman` date NOT NULL,
+  `id_buku` int NOT NULL,
+  PRIMARY KEY (`id_peminjaman`),
+  UNIQUE KEY `id_buku` (`id_buku`),
+  CONSTRAINT `peminjaman_buku` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table db_buku.peminjaman: ~0 rows (approximately)
+
+-- Dumping structure for table db_buku.pengembalian
+CREATE TABLE IF NOT EXISTS `pengembalian` (
+  `id_pengembalian` int NOT NULL,
+  `id_peminjaman` int NOT NULL,
+  `jumlah_denda` int NOT NULL,
+  PRIMARY KEY (`id_pengembalian`),
+  UNIQUE KEY `id_peminjaman` (`id_peminjaman`),
+  CONSTRAINT `pengembalian_buku` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table db_buku.pengembalian: ~0 rows (approximately)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
