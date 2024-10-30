dd14d8d30fcd18780da90f56523ac2f0a2c1f47c
milaaulia21 <milaauliap597@gmail.com>
2024-10-30 09:18:20 +0700

    2

---
+++ src/Controllers/PengembalianController.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+
+use App\Controller;
+use App\Models\BukuModel;
+
+class PengembalianController extends Controller{
+    private $PengembalianModel;
+
+    public function __construct(){
+        $this->PeminjamanModel = new PeminjamanModel();
+    }
+
+    public function list() {
+        $books 
+    }
+}
