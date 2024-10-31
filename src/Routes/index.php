0acca122893a97053084c6605d56fe189b8ef3c6
Bikra123 <bikraabn@gmail.com>
2024-10-31 18:21:19 +0700

    merge main to dendibranch

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -1,10 +1,10 @@
 <?php namespace App\Routes;
 
+use App\Controllers\BukuController;
 use App\Controllers\KategoriController;
 use App\Router;
 
 $router = new Router();
-<<<<<<< HEAD
 $router->get('/',  BukuController::class, 'index');
 $router->post('/', BukuController::class, 'deleteBook');
 
@@ -13,7 +13,6 @@ $router->post('/newBook', BukuController::class, 'addBook');
 
 $router->get('/updateData', BukuController::class, 'updateBook');
 $router->post('/updateData', BukuController::class, 'postBook');
-=======
 $router->get('/kategori', KategoriController::class, 'index');
 
 $router->get('/newCategory', KategoriController::class, 'addCategory');
@@ -23,5 +22,4 @@ $router->post('/kategori', KategoriController::class, 'deleteCategory');
 $router->get('/updatekategori', KategoriController::class, 'updateCategory');
 $router->post('/updatekategori', KategoriController::class, 'postCategory');
 
->>>>>>> 5b8317a22cbdb5b2cb2506ff1ea3f3f6e62b0835
 $router->dispatch();
