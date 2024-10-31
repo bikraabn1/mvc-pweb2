5b8317a22cbdb5b2cb2506ff1ea3f3f6e62b0835
Dendi Ardiansyah <dendiardiansyah61@gmail.com>
2024-10-31 09:54:53 +0700

    remove bikras work

--- src/Routes/index.php
+++ src/Routes/index.php
@@ -1,13 +1,11 @@
 <?php namespace App\Routes;
-use App\Controllers\BukuController;
+
 use App\Controllers\KategoriController;
 use App\Router;
 
 $router = new Router();
-$router->get('/',  BukuController::class, 'index');
 $router->get('/kategori', KategoriController::class, 'index');
-$router->get('/newBook', BukuController::class, 'addBook');
-$router->post('/newBook', BukuController::class, 'addBook');
+
 $router->get('/newCategory', KategoriController::class, 'addCategory');
 $router->post('/newCategory', KategoriController::class, 'addCategory');
 $router->post('/kategori', KategoriController::class, 'deleteCategory');
